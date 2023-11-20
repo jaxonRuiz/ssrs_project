7b3f850fa3f1e526c07bce2f1141548fd265fb41
Jaxon <jacruiz@ucsc.edu>
2023-11-19 16:11:56 -0800

    python script to make character jsons

--- character/char_constructor.py
+++ character/char_constructor.py
@@ -1,12 +1,193 @@
 import json
 
-with open("character\\bin\Debug\\net6.0\emptycharacter.json") as f:
-    raw = json.load(f)
-
-print(type(raw))
-
 def makeCharacter():
-    pass
+    character = {}
+    character['name'] = str(input("name: "))
+    character['hp'] = int(input("hp: "))
+    character['atk'] = int(input("atk: "))
+    character['def'] = int(input("def: "))
+    character['spd'] = int(input("spd: "))
+    character['max-energy'] = int(input("energy: "))
+    character['type'] = str(input("type: "))
+    character['light_cone'] = input("light cone: ")
+    character['equiptment'] = []
+    return character
 
 def makeEquiptment():
-    pass
+    equiptment = {}
+    e_stats = {}
+
+    # valid inputs
+    stat_type = ["flat", "percent", "misc", "elemental"]
+    stat_set = ["hp", "atk", "def", "spd"]
+    misc_stats = ["crt_rate", "crt_dmg", "effect_hit_rate", "effect_resistance", "energy_regen_rate", "break_effect"]
+    valid_types = ['helmet', 'boots', 'chest', 'hands', 'rope', 'orb']
+
+    # setting equiptment basics
+    while True:
+        response = str(input("type: "))
+        if response in valid_types:
+            equiptment["type"] = response
+            break
+        else:
+            print("invalid equiptment type")
+            print(valid_types)
+    equiptment["set"] = str(input("set: "))
+    
+    
+    #oh my god i think i hate python now lol
+    # scuffy looped stat input
+    # running step is going to be acting as a 3 way boolean
+    #   0 asks for flat, percent, etc
+    #   1 enters specified stat
+    #   2 checks for continue
+    #   -1 is end condition
+    print("=====stats=====")
+    running_step = 0
+    while(running_step >= 0): 
+        # begin of cycle
+        for i in stat_type:
+            print(f"{stat_type.index(i)}: [{i}]")
+        response = input("stat type: ")
+
+        # setting flat, percent, misc, elemental type
+        if running_step == 0:
+            if response in stat_type:
+                response = stat_type.index(response)
+            if int(response) in range(len(stat_type)):
+                running_step = 1
+            else:
+                print("incorrect input")
+            # else goes to ask response again
+
+        # filter into flat/percent stats, or misc/elemental
+        if running_step == 1:
+            if int(response) == 0: # flat stat input
+                # printing stat options
+                print()
+                for i in stat_set:
+                    print(f"{stat_set.index(i)}: [{i}]")
+
+                # accounting for stirng inputs
+                base_stat = input("stat: ")
+                if base_stat in stat_set:
+                    base_stat = stat_set.index(base_stat)
+                base_stat = int(base_stat)
+
+                # reading stat type and entering value
+                if int(base_stat) in range(len(stat_set)):
+                    e_stats["_" + stat_set[base_stat]] = int(input("value: "))
+                    running_step = 2
+                elif base_stat in stat_set:
+                    e_stats["%" + base_stat] = int(input("value: "))
+                    running_step = 2
+                else:
+                    print("wrong stat input")
+            elif int(response) == 1: # percent stat input
+                # printing stat options
+                print()
+                for i in stat_set:
+                    print(f"{stat_set.index(i)}: [{i}]")
+
+                # accounting for string input
+                base_stat = input("stat: ")
+                if base_stat in misc_stats:
+                    base_stat = misc_stats.index(base_stat)
+                base_stat = int(base_stat)
+
+                #reading stat type and entering value
+                if int(base_stat) in range(len(stat_set)):
+                    print("\nenter value as a percent, without the percent sign")
+                    e_stats["%" + stat_set[base_stat]] = float(input("value: ")) / 100
+                    running_step = 2
+                elif base_stat in stat_set:
+                    e_stats["%" + base_stat] = float(input("value: ")) / 100
+                    running_step = 2
+                else:
+                    print("wrong stat input")
+
+            elif int(response) == 2: # misc stat input
+                # printing stat options
+                print()
+                for i in misc_stats:
+                    print(f"{misc_stats.index(i)}: [{i}]")
+
+                # accounting for string inputs
+                base_stat = input("stat: ")
+                if base_stat in misc_stats:
+                    base_stat = misc_stats.index(base_stat)
+                base_stat = int(base_stat)
+
+                # reading (misc) stat type and entering value
+                if int(base_stat) in range(len(misc_stats)):
+                    print("\nenter value as a percent, without the percent sign")
+                    e_stats["~" + misc_stats[base_stat]] = float(input("value: ")) / 100
+                    running_step = 2
+                else:
+                    print("wrong stat input")
+
+            elif int(response) == 3: # elemental damage stat 
+                types = ["physical", "fire", "ice", "lightning", "wind", "quantum", "imaginary"]
+                print(types)
+
+                # reading type and entering value
+                type_mult = input("type: ")
+                if type_mult in types:
+                    print("\nenter value as a percent, without the percent sign")
+                    e_stats["!" + type_mult + "_dmg"] = float(input("value: ")) / 100
+
+            else: # error flagger
+                Exception("Error in response reading")
+        
+        # asking to continue loop
+        if running_step == 2:
+            cont = input("Continue? [y/n] ")
+            if cont.lower() == "y" or cont.lower() == "yes":
+                running_step = 0
+            else:
+                # ending loop
+                print("ending here")
+                running_step = -1
+
+    # adding stats to equiptment and returning
+    equiptment["stats"] = e_stats
+    return equiptment
+    
+def createCharFile():
+    output = {"Characters": [], "Loose_equiptment": []}
+
+    print("making characters..")
+    while True:
+        char = makeCharacter()
+        print("\ncharacter created!")
+
+        print("adding equiptment...")
+        while True:
+            char["equiptment"].append(makeEquiptment)
+            if input("continue making equiptment? [y/n]").lower() != "y":
+                break
+            if len(char["equiptment"]) > 6:
+                break
+        output["Characters"].append(char)
+        if input("continue creating characters? [y/n]").lower() != "y":
+            break
+
+    print("making loose equiptment..")
+    while True:
+        output["Loose_equiptment"].append(makeCharacter())
+        print("\nequiptment created!")
+        if input("continue creating equiptment? [y/n]").lower() != "y":
+            break
+
+# print(raw["Characters"][0])
+if __name__ == "__main__":
+    path = "character\\bin\\Debug\\net6.0\\emptycharacter.json"
+    print(path)
+    with open(path) as f:
+        raw = json.load(f)
+
+    print(type(raw))
+    print(raw)
+
+
+    createCharFile()
